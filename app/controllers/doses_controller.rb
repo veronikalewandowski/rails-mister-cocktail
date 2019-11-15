609aa87cbc82ed945f9373f766e51515d94d9341
Veronika Lewandowski <me@veronika-lewandowski.de>
2019-11-15 12:34:17 +0100

    Mr. Cocktail with dose

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -8,7 +8,7 @@ class DosesController < ApplicationController
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
     @dose.cocktail = @cocktail
-    if @dose.save
+    if @dose.save!
       redirect_to cocktail_path(@cocktail)
     else
       render "cocktails/show"
